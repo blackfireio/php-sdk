902589382b2191eee7eda37c268a187ecfacd000
Lucas Pekel <lucas.pekel@gmail.com>
2017-07-27 23:36:35 +0200

    Fixed ReactHttpServer
    ReactHttpServer was outdated and was unable to profile a ReactPHP application.
    
    Signed-off-by: Romain Neutron <imprec@gmail.com>

--- src/Blackfire/Bridge/ReactHttpServer.php
+++ src/Blackfire/Bridge/ReactHttpServer.php
@@ -11,33 +11,63 @@
 
 namespace Blackfire\Bridge;
 
-use React\Http\Request;
-use React\Http\Response;
+use Psr\Http\Message\ServerRequestInterface;
 
+/**
+ * Class ReactHttpServer
+ *
+ * @package Blackfire\Bridge
+ */
 class ReactHttpServer
 {
-    public function handle(Request $request, Response $response)
+    /**
+     * @var \BlackfireProbe
+     */
+    private $probe;
+
+    /**
+     * Start profiling with Blackfire if header is present
+     *
+     * @param ServerRequestInterface $request
+     */
+    public function handleRequest(ServerRequestInterface $request)
     {
         $headers = array_change_key_case($request->getHeaders(), CASE_LOWER);
 
         // Only enable when the X-Blackfire-Query header is present
         if (!isset($headers['x-blackfire-query'])) {
-            return array();
+            return;
         }
 
-        $probe = new \BlackfireProbe($headers['x-blackfire-query']);
-
+        $this->probe = new \BlackfireProbe($headers['x-blackfire-query'][0]);
         // Stop if it failed
-        if (!$probe->enable()) {
-            return array();
+        if (!$this->probe->enable()) {
+            fwrite(STDERR, 'Could not start blackfire probe');
+
+            return;
+        }
+    }
+
+    /**
+     * Stop profiling and add Blackfire headers if needed
+     *
+     * @param array $headers
+     * @return array
+     */
+    public function handleHeaders($headers)
+    {
+        if ($this->probe) {
+            $this->probe->close();
+
+            // Return the header
+            $header = explode(':', $this->probe->getResponseLine(), 2);
+
+            $headers['x-' . $header[0]] = $header[1];
+
+            $this->probe = null;
         }
 
-        // Stop profiling once the request ends
-        $response->on('end', array($probe, 'close'));
-
-        // Return the header
-        $header = explode(':', $probe->getResponseLine(), 2);
-
-        return array('x-'.$header[0] => $header[1]);
+        return $headers;
     }
+
 }
