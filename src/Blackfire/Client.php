42cca1502619cb9e4912dfcc447c8e03366c3676
Nicolas Grekas <nicolas.grekas@gmail.com>
2017-12-11 18:07:54 +0100

    [sdk] get through HTTP proxies

--- src/Blackfire/Client.php
+++ src/Blackfire/Client.php
@@ -448,7 +448,7 @@ class Client
             $sslOpts['cafile'] = $caPath;
         }
 
-        $context = stream_context_create([
+        $context = self::getContext([
             'http' => array_replace([
                 'method' => $method,
                 'header' => implode("\r\n", $headers),
@@ -494,5 +494,151 @@ class Client
         }
 
         return $body;
+    }
+
+    /**
+     * Creates a context supporting HTTP proxies
+     *
+     * The following method is copy/pasted from Composer v1.5.5
+     *
+     * Copyright (c) Nils Adermann, Jordi Boggiano
+     *
+     * Permission is hereby granted, free of charge, to any person obtaining a copy
+     * of this software and associated documentation files (the "Software"), to deal
+     * in the Software without restriction, including without limitation the rights
+     * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+     * copies of the Software, and to permit persons to whom the Software is furnished
+     * to do so, subject to the following conditions:
+     *
+     * The above copyright notice and this permission notice shall be included in all
+     * copies or substantial portions of the Software.
+     *
+     * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+     * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+     * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+     * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+     * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+     * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+     * THE SOFTWARE.
+     *
+     * @param  string            $url            URL the context is to be used for
+     * @param  array             $defaultOptions Options to merge with the default
+     * @param  array             $defaultParams  Parameters to specify on the context
+     * @throws \RuntimeException if https proxy required and OpenSSL uninstalled
+     * @return resource          Default context
+     *
+     * @author Jordan Alliot <jordan.alliot@gmail.com>
+     * @author Markus Tacker <m@coderbyheart.de>
+     */
+    private static function getContext($url, array $defaultOptions = array(), array $defaultParams = array())
+    {
+        $options = array('http' => array(
+            // specify defaults again to try and work better with curlwrappers enabled
+            'follow_location' => 1,
+            'max_redirects' => 20,
+        ));
+
+        // Handle HTTP_PROXY/http_proxy on CLI only for security reasons
+        if (PHP_SAPI === 'cli' && (!empty($_SERVER['HTTP_PROXY']) || !empty($_SERVER['http_proxy']))) {
+            $proxy = parse_url(!empty($_SERVER['http_proxy']) ? $_SERVER['http_proxy'] : $_SERVER['HTTP_PROXY']);
+        }
+
+        // Prefer CGI_HTTP_PROXY if available
+        if (!empty($_SERVER['CGI_HTTP_PROXY'])) {
+            $proxy = parse_url($_SERVER['CGI_HTTP_PROXY']);
+        }
+
+        // Override with HTTPS proxy if present and URL is https
+        if (preg_match('{^https://}i', $url) && (!empty($_SERVER['HTTPS_PROXY']) || !empty($_SERVER['https_proxy']))) {
+            $proxy = parse_url(!empty($_SERVER['https_proxy']) ? $_SERVER['https_proxy'] : $_SERVER['HTTPS_PROXY']);
+        }
+
+        // Remove proxy if URL matches no_proxy directive
+        if (!empty($_SERVER['no_proxy']) && parse_url($url, PHP_URL_HOST)) {
+            $pattern = new NoProxyPattern($_SERVER['no_proxy']);
+            if ($pattern->test($url)) {
+                unset($proxy);
+            }
+        }
+
+        if (!empty($proxy)) {
+            $proxyURL = isset($proxy['scheme']) ? $proxy['scheme'] . '://' : '';
+            $proxyURL .= isset($proxy['host']) ? $proxy['host'] : '';
+
+            if (isset($proxy['port'])) {
+                $proxyURL .= ":" . $proxy['port'];
+            } elseif ('http://' == substr($proxyURL, 0, 7)) {
+                $proxyURL .= ":80";
+            } elseif ('https://' == substr($proxyURL, 0, 8)) {
+                $proxyURL .= ":443";
+            }
+
+            // http(s):// is not supported in proxy
+            $proxyURL = str_replace(array('http://', 'https://'), array('tcp://', 'ssl://'), $proxyURL);
+
+            if (0 === strpos($proxyURL, 'ssl:') && !extension_loaded('openssl')) {
+                throw new \RuntimeException('You must enable the openssl extension to use a proxy over https');
+            }
+
+            $options['http']['proxy'] = $proxyURL;
+
+            // enabled request_fulluri unless it is explicitly disabled
+            switch (parse_url($url, PHP_URL_SCHEME)) {
+                case 'http': // default request_fulluri to true
+                    $reqFullUriEnv = getenv('HTTP_PROXY_REQUEST_FULLURI');
+                    if ($reqFullUriEnv === false || $reqFullUriEnv === '' || (strtolower($reqFullUriEnv) !== 'false' && (bool) $reqFullUriEnv)) {
+                        $options['http']['request_fulluri'] = true;
+                    }
+                    break;
+                case 'https': // default request_fulluri to true
+                    $reqFullUriEnv = getenv('HTTPS_PROXY_REQUEST_FULLURI');
+                    if ($reqFullUriEnv === false || $reqFullUriEnv === '' || (strtolower($reqFullUriEnv) !== 'false' && (bool) $reqFullUriEnv)) {
+                        $options['http']['request_fulluri'] = true;
+                    }
+                    break;
+            }
+
+            // add SNI opts for https URLs
+            if ('https' === parse_url($url, PHP_URL_SCHEME)) {
+                $options['ssl']['SNI_enabled'] = true;
+                if (PHP_VERSION_ID < 50600) {
+                    $options['ssl']['SNI_server_name'] = parse_url($url, PHP_URL_HOST);
+                }
+            }
+
+            // handle proxy auth if present
+            if (isset($proxy['user'])) {
+                $auth = urldecode($proxy['user']);
+                if (isset($proxy['pass'])) {
+                    $auth .= ':' . urldecode($proxy['pass']);
+                }
+                $auth = base64_encode($auth);
+
+                // Preserve headers if already set in default options
+                if (isset($defaultOptions['http']['header'])) {
+                    if (is_string($defaultOptions['http']['header'])) {
+                        $defaultOptions['http']['header'] = array($defaultOptions['http']['header']);
+                    }
+                    $defaultOptions['http']['header'][] = "Proxy-Authorization: Basic {$auth}";
+                } else {
+                    $options['http']['header'] = array("Proxy-Authorization: Basic {$auth}");
+                }
+            }
+        }
+
+        $options = array_replace_recursive($options, $defaultOptions);
+
+        if (isset($options['http']['header'])) {
+            if (!is_array($header = $options['http']['header'])) {
+                $header = explode("\r\n", $header);
+            }
+            uasort($header, function ($el) {
+                return preg_match('{^content-type}i', $el) ? 1 : -1;
+            });
+
+            $options['http']['header'] = $header;
+        }
+
+        return stream_context_create($options, $defaultParams);
     }
 }
