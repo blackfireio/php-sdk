bb1cd9bf0c668d0522c6c92ab165d456018f99b9
Fabien Potencier <fabien.potencier@gmail.com>
2018-01-25 12:13:50 +0100

    changed (c) to Blackfire

--- src/Blackfire/Exception/EnvNotFoundException.php
+++ src/Blackfire/Exception/EnvNotFoundException.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of the Blackfire SDK package.
  *
- * (c) SensioLabs <contact@sensiolabs.com>
+ * (c) Blackfire <support.io>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
