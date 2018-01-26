89e4941a3b084449591d35b2175d587156b605a2
Fabien Potencier <fabien.potencier@gmail.com>
2018-01-26 16:30:15 +0100

    fixed (c)

--- src/Blackfire/Bridge/Guzzle/Middleware.php
+++ src/Blackfire/Bridge/Guzzle/Middleware.php
@@ -5,7 +5,7 @@ namespace Blackfire\Bridge\Guzzle;
 /*
  * This file is part of the Blackfire SDK package.
  *
- * (c) Blackfire <support.io>
+ * (c) Blackfire <support@blackfire.io>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
--- src/Blackfire/Exception/ConfigNotFoundException.php
+++ src/Blackfire/Exception/ConfigNotFoundException.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of the Blackfire SDK package.
  *
- * (c) Blackfire <support.io>
+ * (c) Blackfire <support@blackfire.io>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
