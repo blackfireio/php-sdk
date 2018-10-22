78fee1aa2d30655059bc528af23e4b01367cc95f
iamluc <luc@vieillescazes.net>
2018-10-22 15:57:29 +0200

    Remove headers from ApiException's message

--- src/Blackfire/Exception/ApiException.php
+++ src/Blackfire/Exception/ApiException.php
@@ -13,6 +13,15 @@ namespace Blackfire\Exception;
 
 class ApiException extends RuntimeException
 {
+    private $headers;
+
+    public function __construct($message = '', $code = 0, \Exception $previous = null, $headers = array())
+    {
+        parent::__construct($message, $code, $previous);
+
+        $this->headers = $headers;
+    }
+
     public static function fromStatusCode($message, $code, \Exception $previous = null)
     {
         return new static(sprintf('%s: %s', $code, $message), $code, $previous);
@@ -20,6 +29,11 @@ class ApiException extends RuntimeException
 
     public static function fromURL($method, $url, $message, $code, $context, $headers, \Exception $previous = null)
     {
-        return new static(sprintf('%s: %s while calling %s %s [context: %s] [headers: %s]', $code, $message, $method, $url, var_export($context, true), var_export($headers, true)), $code, $previous);
+        return new static(sprintf('%s: %s while calling %s %s [context: %s]', $code, $message, $method, $url, var_export($context, true)), $code, $previous, $headers);
+    }
+
+    public function getHeaders()
+    {
+        return $this->headers;
     }
 }
