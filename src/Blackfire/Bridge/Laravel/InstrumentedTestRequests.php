db3ee43a54d9a242cbcfe78ef987114454898211
Thomas di Luccio <thomas.diluccio@blackfire.io>
2021-12-14 15:23:54 +0100

    Add doc for the CLI commands and fix typos

--- src/Blackfire/Bridge/Laravel/InstrumentedTestRequests.php
+++ src/Blackfire/Bridge/Laravel/InstrumentedTestRequests.php
@@ -1,5 +1,15 @@
 <?php
 
+/*
+ * This file is part of the Blackfire SDK package.
+ *
+ * (c) Blackfire <support@blackfire.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+
 namespace Blackfire\Bridge\Laravel;
 
 use Closure;
