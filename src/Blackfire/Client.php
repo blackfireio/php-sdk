eadb6ac062198b45cbadcf96bb9760494d522aed
Romain Neutron <romain@neutron.io>
2022-05-19 09:35:20 +0200

    Bump PHP SDK to 1.30.0

--- src/Blackfire/Client.php
+++ src/Blackfire/Client.php
@@ -29,7 +29,7 @@ class Client
 {
     const MAX_RETRY = 60;
     const NO_REFERENCE_ID = '00000000-0000-0000-0000-000000000000';
-    const VERSION = '1.29.0';
+    const VERSION = '1.30.0';
 
     private $config;
     private $collabTokens;
